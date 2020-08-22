15521335ce33fd965f3a2392b63a4eccdbec8eb9
杨凤玲 <yangfengling@inttech.cn>
2020-08-22 10:56:22 +0800

    pytorch 线性回归模型参数相关的方法

--- 1-线性回归模型.py
+++ 1-线性回归模型.py
@@ -17,7 +17,7 @@ class LinearModel(nn.Module):
 
 lm = LinearModel(5)  # 定义线性回归模型，特征数为5
 x = torch.randn(4, 5)  # 定义随机输入，迷你批次大小为4
-print(lm(x))  # 得到每个迷你批次的输出
+print(lm(x))  # 得到每个迷你批次的输出  根据模型获取对应的输出
 
 '''
 输出结果如下所示：
@@ -25,4 +25,19 @@ tensor([[ 0.0997],
         [ 1.9427],
         [ 1.6200],
         [-0.3356]], grad_fn=<AddBackward0>)
+'''
+
+print(lm.named_parameters())  # 获取模型参数(带名字)的生成器
+# 输出 <generator object Module.named_parameters at 0x0000017A7FABE048>
+
+print(list(lm.named_parameters())) # 转换生成器为列表
+'''
+输出结果如下所示：
+[('weight', Parameter containing:
+tensor([[-0.3262],
+        [ 1.0083],
+        [-1.4525],
+        [ 0.7031],
+        [ 0.3558]], requires_grad=True)), ('bias', Parameter containing:
+tensor([-1.5573], requires_grad=True))]
 '''
