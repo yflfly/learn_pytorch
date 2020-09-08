cabfeece306e76487e73d9b302cb12bc5744979c
杨凤玲 <yangfengling@inttech.cn>
2020-09-08 17:47:38 +0800

    code

---
+++ pytorch常用代码段集合/3-命名张量.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+import torch
+
+# Tensor[N, C, H, W]
+images = torch.randn(32, 3, 56, 56)
+images.sum(dim=1)
+images.select(dim=1, index=0)
+
+# PyTorch 1.3之后
+
+NCHW = ['N', 'C', 'H', 'W']
+images = torch.randn(32, 3, 56, 56, names=NCHW)
+images.sum('C')
+images.select('C', index=0)
+# 也可以这么设置
+tensor = torch.rand(3, 4, 1, 2, names=('C', 'N', 'H', 'W'))
+# 使用align_to可以对维度方便地排序
+tensor = tensor.align_to('N', 'C', 'H', 'W')
