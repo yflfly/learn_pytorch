5ce21666ff7ea1b835d3231a2964cce2beda142a
杨凤玲 <yangfengling@inttech.cn>
2020-09-09 08:48:09 +0800

    LSTM

---
+++ 深入浅出PyTorch从模型到源码/example-LSTM.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import torch
+import torch.nn as nn
+
+rnn = nn.LSTM(10, 20, 2)  # 10：词向量的维度  20：隐含变量的维度大小  2：循环神经网络的层数
+input = torch.randn(5, 3, 10)  # 序列对应的张量，序列的长度*迷你批次的大小*输入的特征数目
+h0 = torch.randn(2, 3, 20)  # 初始的隐状态 2：循环神经网络的层数 3：迷你批次大小  20：隐含层的维度
+c0 = torch.randn(2, 3, 20)  # 初始的隐状态 2：循环神经网络的层数 3：迷你批次大小  20：隐含层的维度
+output, (hn, cn) = rnn(input, (h0, c0))
+
+print(rnn)
+print(input.shape)
+print(h0.shape)
+print(c0.shape)
+print(output.shape)
+print(hn.shape)
+print(cn.shape)
+'''
+课本P277
+输出结果如下所示：
+LSTM(10, 20, num_layers=2)
+torch.Size([5, 3, 10])
+torch.Size([2, 3, 20])
+torch.Size([2, 3, 20])
+torch.Size([5, 3, 20])
+torch.Size([2, 3, 20])
+torch.Size([2, 3, 20])
+'''
