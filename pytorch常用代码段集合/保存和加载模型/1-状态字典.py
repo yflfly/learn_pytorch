9950bf19fa4df4b06bc5b11b7edf9bcfef7c714d
杨凤玲 <yangfengling@inttech.cn>
2021-02-20 15:03:38 +0800

    Pytorch-保存和加载模型

--- pytorch常用代码段集合/保存和加载模型/1-状态字典.py
+++ pytorch常用代码段集合/保存和加载模型/1-状态字典.py
@@ -24,3 +24,39 @@ class TheModelClass(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+# 初始化模型
+model = TheModelClass()
+
+# 初始化优化器
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# 打印模型的状态字典
+print("Model's state_dict:")
+for param_tensor in model.state_dict():
+    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+# 打印优化器的状态字典
+print("Optimizer's state_dict:")
+for var_name in optimizer.state_dict():
+    print(var_name, "\t", optimizer.state_dict()[var_name])
+
+'''
+输出结果如下所示：
+Model's state_dict:
+conv1.weight 	 torch.Size([6, 3, 5, 5])
+conv1.bias 	 torch.Size([6])
+conv2.weight 	 torch.Size([16, 6, 5, 5])
+conv2.bias 	 torch.Size([16])
+fc1.weight 	 torch.Size([120, 400])
+fc1.bias 	 torch.Size([120])
+fc2.weight 	 torch.Size([84, 120])
+fc2.bias 	 torch.Size([84])
+fc3.weight 	 torch.Size([10, 84])
+fc3.bias 	 torch.Size([10])
+Optimizer's state_dict:
+state 	 {}
+param_groups 	 [{'lr': 0.001, 'momentum': 0.9, 'dampening': 0, 'weight_decay': 0, 'nesterov': False, 'params': [2091698046080, 2091698046152, 2091698046224, 2091698046296, 2091698046368, 2091698046440, 2091698046512, 2091698046584, 2091698046728, 2091698046800]}]
+
+'''
