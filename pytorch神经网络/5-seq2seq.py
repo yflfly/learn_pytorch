6b02d5596f806ff93c594c8c7220c01311d7a1e3
杨凤玲 <yangfengling@inttech.cn>
2021-02-22 11:27:51 +0800

    Pytorch-seq2seq

--- pytorch神经网络/5-seq2seq.py
+++ pytorch神经网络/5-seq2seq.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import random
 
 
 class Encoder(nn.Module):
@@ -126,6 +127,53 @@ class Decoder(nn.Module):
         pred = self.fc_out(torch.cat((dec_output, c, embedded), dim=1))
 
         return pred, dec_hidden.squeeze(0)
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        # src = [src_len, batch_size]
+        # trg = [trg_len, batch_size]
+        # teacher_forcing_ratio is probability to use teacher forcing
+
+        batch_size = src.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        # tensor to store decoder outputs
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+
+        # enc_output is all hidden states of the input sequence, back and forwards
+        # s is the final forward and backward hidden states, passed through a linear layer
+        enc_output, s = self.encoder(src)
+
+        # first input to the decoder is the <sos> tokens
+        dec_input = trg[0, :]
+
+        for t in range(1, trg_len):
+            # insert dec_input token embedding, previous hidden state and all encoder hidden states
+            # receive output tensor (predictions) and new hidden state
+            dec_output, s = self.decoder(dec_input, s, enc_output)
+
+            # place predictions in a tensor holding predictions for each token
+            outputs[t] = dec_output
+
+            # decide if we are going to use teacher forcing or not
+            teacher_force = random.random() < teacher_forcing_ratio
+
+            # get the highest predicted token from our predictions
+            top1 = dec_output.argmax(1)
+
+            # if teacher forcing, use actual next token as next input
+            # if not, use predicted token
+            dec_input = trg[t] if teacher_force else top1
+
+        return outputs
 
 
 '''
