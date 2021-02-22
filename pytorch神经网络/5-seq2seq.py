a5abf1cf40b451eb9640366fda7becc8bf24d6a1
杨凤玲 <yangfengling@inttech.cn>
2021-02-22 11:15:30 +0800

    Pytorch-seq2seq

---
+++ pytorch神经网络/5-seq2seq.py
@@ -0,0 +1,134 @@
+# coding:utf-8
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
+        super().__init__()
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn = nn.GRU(emb_dim, enc_hid_dim, bidirectional=True)
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        '''
+        src = [src_len, batch_size]  输入的src源语言数据维度为 [batch, src_len]
+        '''
+        src = src.transpose(0, 1)  # src = [batch_size, src_len]
+        embedded = self.dropout(self.embedding(src)).transpose(0, 1)  # embedded = [src_len, batch_size, emb_dim]
+        # 经过embedding层得到输出(调换维度，把batch放到第二个位置)
+
+        # enc_output = [src_len, batch_size, hid_dim * num_directions]
+        # enc_hidden = [n_layers * num_directions, batch_size, hid_dim]
+        enc_output, enc_hidden = self.rnn(embedded)  # if h_0 is not give, it will be set 0 acquiescently
+
+        # enc_hidden is stacked [forward_1, backward_1, forward_2, backward_2, ...]
+        # enc_output are always from the last layer
+
+        # enc_hidden [-2, :, : ] is the last of the forwards RNN 正向隐藏状态
+        # enc_hidden [-1, :, : ] is the last of the backwards RNN 反向隐藏状态
+
+        # initial decoder hidden is final hidden state of the forwards and backwards
+        # encoder RNNs fed through a linear layer
+        # s = [batch_size, dec_hid_dim]
+        # 把双向gru最后一个时刻正向隐藏状态和反向隐藏状态拼接起来送进fc全连接网络得到S0
+        s = torch.tanh(self.fc(torch.cat((enc_hidden[-2, :, :], enc_hidden[-1, :, :]), dim=1)))
+
+        return enc_output, s
+
+
+'''
+attention部分主要就是计算某一个时刻decoder的S与encoder隐藏状态的相关性
+'''
+
+
+class Attention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim, bias=False)
+        self.v = nn.Linear(dec_hid_dim, 1, bias=False)
+
+    def forward(self, s, enc_output):
+        # s = [batch_size, dec_hid_dim] S0
+        # enc_output = [src_len, batch_size, enc_hid_dim * 2]
+
+        batch_size = enc_output.shape[1]
+        src_len = enc_output.shape[0]
+
+        # repeat decoder hidden state src_len times  把s0变为[batch, src_len, dec_hid_dim]
+        # s = [batch_size, src_len, dec_hid_dim]
+        # enc_output = [batch_size, src_len, enc_hid_dim * 2]
+        s = s.unsqueeze(1).repeat(1, src_len, 1)
+        enc_output = enc_output.transpose(0, 1)
+        # [src_len, batch_size, enc_hid_dim * 2] ---> [batch_size, src_len, enc_hid_dim * 2]
+
+        # energy = [batch_size, src_len, dec_hid_dim] 拼接s0和enc_output并送进全连接网络
+        energy = torch.tanh(self.attn(torch.cat((s, enc_output), dim=2)))
+
+        # attention = [batch_size, src_len]
+        attention = self.v(energy).squeeze(2)
+
+        return F.softmax(attention, dim=1)
+
+
+'''
+decoder也是一个gru，但是不同于encoder的是这个一个单层单向的gru
+'''
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout, attention):
+        super().__init__()
+        self.output_dim = output_dim
+        self.attention = attention  # attention层，decoder需要调用attention类计算相关性
+        self.embedding = nn.Embedding(output_dim, emb_dim)  # embedding层,这里的embedding是对目标语言做词嵌入
+        # gru层，把attention层结果a与enc_output相乘得到结果c，再把c与此时刻的dec_input拼接送入gru
+        self.rnn = nn.GRU((enc_hid_dim * 2) + emb_dim, dec_hid_dim)
+        self.fc_out = nn.Linear((enc_hid_dim * 2) + dec_hid_dim + emb_dim, output_dim)
+        # fc_out层，其实就是做一个output_dim类的分类器，输出此时刻decoder的输出
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, dec_input, s, enc_output):
+        # dec_input = [batch_size]
+        # s = [batch_size, dec_hid_dim]
+        # enc_output = [src_len, batch_size, enc_hid_dim * 2]
+
+        dec_input = dec_input.unsqueeze(1)  # dec_input = [batch_size, 1]
+
+        embedded = self.dropout(self.embedding(dec_input)).transpose(0, 1)  # embedded = [1, batch_size, emb_dim]
+
+        # a = [batch_size, 1, src_len]
+        a = self.attention(s, enc_output).unsqueeze(1)
+
+        # enc_output = [batch_size, src_len, enc_hid_dim * 2]
+        enc_output = enc_output.transpose(0, 1)
+
+        # c = [1, batch_size, enc_hid_dim * 2]
+        c = torch.bmm(a, enc_output).transpose(0, 1)
+
+        # rnn_input = [1, batch_size, (enc_hid_dim * 2) + emb_dim]
+        rnn_input = torch.cat((embedded, c), dim=2)
+
+        # dec_output = [src_len(=1), batch_size, dec_hid_dim]
+        # dec_hidden = [n_layers * num_directions, batch_size, dec_hid_dim]
+        dec_output, dec_hidden = self.rnn(rnn_input, s.unsqueeze(0))
+
+        # embedded = [batch_size, emb_dim]
+        # dec_output = [batch_size, dec_hid_dim]
+        # c = [batch_size, enc_hid_dim * 2]
+        embedded = embedded.squeeze(0)
+        dec_output = dec_output.squeeze(0)
+        c = c.squeeze(0)
+
+        # pred = [batch_size, output_dim]
+        pred = self.fc_out(torch.cat((dec_output, c, embedded), dim=1))
+
+        return pred, dec_hidden.squeeze(0)
+
+
+'''
+代码参考网址：
+https://blog.csdn.net/qq_43238260/article/details/112425230
+'''
